5dba7e6ac9c932d35f0fb6b4a74c975a9f2b9281
Rahman Younus <rahman_95@live.co.uk>
2022-02-20 20:28:09 +0000

    try

--- tests/config/cypress/support/index.js
+++ tests/config/cypress/support/index.js
@@ -1,1 +1,6 @@
 import '@testing-library/cypress/add-commands';
+
+Cypress.Commands.add('visitApp', () => {
+  const url = process.env.DEPLOY_URL || 'localhost:3000'
+	cy.visit(url)
+})
--- tests/ui/all-messages.spec.js
+++ tests/ui/all-messages.spec.js
@@ -1,31 +1,31 @@
 describe('All Messages', () => {
   before(() => {
-    cy.visit('localhost:3000')
-  })
+    cy.visitApp();
+  });
 
   it('should be on correct page', () => {
-    cy.contains('React Redux Chat').should('exist')
-    cy.contains('All Messages').should('exist')
-    cy.contains('Loading...').should('exist')
-  })
+    cy.contains('React Redux Chat').should('exist');
+    cy.contains('All Messages').should('exist');
+    cy.contains('Loading...').should('exist');
+  });
 
   it('should be able to find correct first message', () => {
     cy.findAllByTestId('message').first().within(() => {
-      cy.contains('Amanda Baker')
-      cy.contains('Mauris lacinia sapien quis libero.')
-      cy.contains('27 Feb 2017 13:47')
-    })
-  })
+      cy.contains('Amanda Baker');
+      cy.contains('Mauris lacinia sapien quis libero.');
+      cy.contains('27 Feb 2017 13:47');
+    });
+  });
 
   it('should be able to scroll to the bottom', () => {
-    cy.findByTestId('messages').scrollTo('bottom')
-  })
+    cy.findByTestId('messages').scrollTo('bottom');
+  });
 
   it('should be able to find correct last message', () => {
     cy.findAllByTestId('message').last().within(() => {
-      cy.contains('Amanda Baker')
-      cy.contains('Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi.')
-      cy.contains('24 Feb 2016 00:01')
-    })
-  })
-})
+      cy.contains('Amanda Baker');
+      cy.contains('Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi.');
+      cy.contains('24 Feb 2016 00:01');
+    });
+  });
+});
