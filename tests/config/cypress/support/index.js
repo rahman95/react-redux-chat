1afdf414e182e56c20abb1eb72fcd49053f46b37
Rahman Younus <rahman_95@live.co.uk>
2022-02-20 22:18:07 +0000

    try

--- tests/config/cypress/support/index.js
+++ tests/config/cypress/support/index.js
@@ -1,6 +1,6 @@
 import '@testing-library/cypress/add-commands'
 
 Cypress.Commands.add('visitApp', () => {
-  const url = cy.env('deploy_url') || 'localhost:3000'
+  const url = Cypress.env('deploy_url') || 'localhost:3000'
   cy.visit(url)
 })
