5c8bfc3a819f706386c450174f876907458c80d1
Rahman Younus <rahman_95@live.co.uk>
2022-02-20 14:55:41 +0000

    got main tasks done

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import dayjs from 'dayjs';
+import { useSelector } from 'react-redux';
+
+const DEFAULT_AVATAR = 'http://dummyimage.com/100x100.png/5fa2dd/ffffff';
 
 const Message = ({ message }) => {
-  const { id, message: messageText, timestamp, userId } = message;
+  const { members } = useSelector((state) => state.members);
+  const { message: messageText, timestamp, userId } = message;
+  const parseTs = dayjs(timestamp).format('DD MMM YYYY HH:mm');
+
+  const user = members.find((member) => member.id === userId);
+  const name = `${user?.firstName} ${user?.lastName}` || ''
+  const email = user?.email || ''
+  const avatar = user?.avatar || DEFAULT_AVATAR;
+
   return (
-    <div className="message">
+    <div className="message" title={email}>
       <div className="user">
-        <img className="avatar" src="http://dummyimage.com/100x100.png/5fa2dd/ffffff"></img>
+        <img className="avatar" src={avatar}></img>
       </div>
       <div className="text">
-        <span>{userId}</span>
-        <p>{messageText}</p>
+        <span>{name}</span>
+        <p >{messageText}</p>
+        <span className="ts">{parseTs}</span>
       </div>
-      {/* <span>Id: {id}</span> */}
-      {/* <span>Ts: {timestamp}</span> */}
     </div>
   );
 };
--- src/components/MessageContainer.js
+++ src/components/MessageContainer.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import dayjs from 'dayjs';
 import Message from './Message';
 
 const MessageContainer = ({ loading, messages }) => {
+  const sortedMessages = messages
+    .sort((a, b) => dayjs(b.timestamp) - dayjs(a.timestamp))
+    .map((message, index) => <Message message={message} key={index} />);
+
   return (
     <div className="message-container">
       <div className="title">
         <h3>All Messages</h3>
       </div>
-      <div className="messages">
-        {loading ? <span>Loading...</span> : messages.map((message, index) => <Message message={message} key={index} />)}
-      </div>
+      <div className="messages">{loading ? <span>Loading...</span> : sortedMessages}</div>
     </div>
   );
 };
